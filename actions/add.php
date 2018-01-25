c7cd8bf8e1dfa710ba931dbaa01de771c5441aec
Carlos Santos <carlos@one-shift.com>
2018-01-25 10:15:31 +0000

    v0.0.2

--- actions/add.php
+++ actions/add.php
@@ -3,20 +3,17 @@
 if(!isset($_POST['save'])) {
 	$form_tpl = bo3::mdl_load("templates-e/add/form.tpl");
 
-	$mdl = bo3::c2r(
-		[
-			'content' => $form_tpl,
-			'lg-name' => $mdl_lang['form']['name'],
-			'name-placeholder' => $mdl_lang['form']['name-placeholder'],
-			'lg-sort' => $mdl_lang['form']['sort'],
-			'sort-placeholder' => $mdl_lang['form']['sort-placeholder'],
-			'lg-code' => $mdl_lang['form']['code'],
-			'code-placeholder' => $mdl_lang['form']['code-placeholder'],
-			'lg-status' => $mdl_lang['form']['status'],
-			'save-btn' => $mdl_lang['form']['save']
-		],
-		bo3::mdl_load("templates/add.tpl")
-	);
+	$mdl = bo3::c2r([
+		'content' => $form_tpl,
+		'lg-name' => $mdl_lang['form']['name'],
+		'name-placeholder' => $mdl_lang['form']['name-placeholder'],
+		'lg-sort' => $mdl_lang['form']['sort'],
+		'sort-placeholder' => $mdl_lang['form']['sort-placeholder'],
+		'lg-code' => $mdl_lang['form']['code'],
+		'code-placeholder' => $mdl_lang['form']['code-placeholder'],
+		'lg-status' => $mdl_lang['form']['status'],
+		'save-btn' => $mdl_lang['form']['save']
+	], bo3::mdl_load("templates/add.tpl"));
 } else {
 	$rank = new rank();
 	$rank->setName($_POST['name']);
@@ -27,26 +24,24 @@ if(!isset($_POST['save'])) {
 	$rank->setStatus(isset($_POST["status"]) ? $_POST["status"] : 0);
 
 	if($rank->insert()) {
-		$toReturn = bo3::c2r(
-			[
-				'message' => $mdl_lang['add']['success'],
-				'type' =>  'success'
-			],
-			bo3::mdl_load('templates-e/add/message.tpl')
-		);
-		header( "refresh:2;url={$cfg->system->path_bo}/{$lg_s}/ranks/" );
+		$toReturn = bo3::c2r([
+			'message' => $mdl_lang['add']['success'],
+			'type' =>  'success'
+		], bo3::mdl_load('templates-e/add/message.tpl'));
+		header( "refresh:2;url={$cfg->system->path_bo}/{$lg_s}/11-ranks/" );
 	} else {
-		$toReturn = bo3::c2r(
-			[
-				'message' => $mdl_lang['add']['failure'],
-				'type' =>  'danger'
-			],
-			bo3::mdl_load('templates-e/add/message.tpl')
-		);
-		header( "refresh:5;url={$cfg->system->path_bo}/{$lg_s}/ranks/");
+		$toReturn = bo3::c2r([
+			'message' => $mdl_lang['add']['failure'],
+			'type' =>  'danger'
+		], bo3::mdl_load('templates-e/add/message.tpl'));
+		header( "refresh:5;url={$cfg->system->path_bo}/{$lg_s}/11-ranks/");
 	}
 
 	$mdl = bo3::c2r(['content' => $toReturn], bo3::mdl_load('templates/add.tpl'));
 }
+
+$breadcrumb = [
+	["name" => "Add", "link" => "{c2r-path-bo}/{c2r-lg}/{c2r-module-folder}/add/"]
+];
 
 include "pages/module-core.php";
--- db/install.sql
+++ db/install.sql
@@ -1,10 +1,12 @@
 INSERT INTO `{c2r-prefix}_modules` (`name`, `folder`, `code`, `sort`) VALUES ("{c2r-mod-name}", "{c2r-mod-folder}", "{c2r-mod-code}", 0);
 
-CREATE TABLE IF NOT EXISTS `{c2r-prefix}_example` (
+CREATE TABLE `{c2r-prefix}_ranks` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`name` varchar(255) NOT NULL,
-	`folder` varchar(255) NOT NULL,
-	`sort` int(11) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`id`),
-	UNIQUE KEY `folder` (`folder`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+	`code` text NOT NULL,
+	`sort` int(11) NOT NULL,
+	`date` datetime NOT NULL,
+	`date_update` datetime NOT NULL,
+	`status` tinyint(1) NOT NULL,
+	`user_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
